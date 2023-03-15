549f9c36b7a32c21055e82155f355b9c0193ed80
Isaac Freires <isaac.freires@osf.digital>
2023-03-15 14:50:33 -0300

    Responsividade no Navbar; Atualização aba Sobre

--- action_max/src/Components/Layout/Navbar.js
+++ action_max/src/Components/Layout/Navbar.js
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom"
-
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { useRef } from "react"
+ 
 //import Container from "./Container"
 
 import styles from './Navbar.module.css'
@@ -9,32 +11,47 @@ import iconPerson from '../../Img/iconPersonagens.jpg'
 import iconSobre from '../../Img/iconSobre.png'
 
 function Navbar(){
+    const navRef = useRef();
+
+    const showBarNav = () => {
+        navRef.current.classList.toggle(styles.nav_responsive);
+    }
+
+
     return (
-      <nav className={styles.navbar}>
-            <Link to='/'>
-                <img src={logo} alt="Marvel" className={styles.logo} />
-            </Link>
-            <ul className={styles.list}>
-                <li className={`${styles.item} ${styles.pags}`}>
-                    <Link to='/' className={styles.link}>
-                        <img src={iconHome} alt="Icon Home" className={styles.imgHome} />
-                        <span>Home</span>
-                    </Link>
-                </li>
-                <li className={`${styles.item} ${styles.pags}`}>
-                    <Link to='/personagens' className={styles.link}>
-                        <img src={iconPerson} alt="Icon Person" className={styles.imgPerson} />
-                        <span>Personagens</span>
-                    </Link>
-                </li>
-                <li className={`${styles.item} ${styles.pags}`}>
-                    <Link to='/sobre' className={styles.link}>
-                        <img src={iconSobre} alt="Icon Sobre" className={styles.imgHome} />
-                        <span>Sobre</span>
-                    </Link>
-                </li>
-            </ul>
-      </nav>
+        <header>
+            <nav className={styles.navbar}ref={navRef} >
+                <Link to='/'>
+                    <img src={logo} alt="Marvel" className={styles.logo} />
+                </Link>
+                <ul className={styles.list}>
+                    <li className={`${styles.item} ${styles.pags}`}>
+                        <Link to='/' className={styles.link}>
+                            <img src={iconHome} alt="Icon Home" className={styles.imgHome} />
+                            <span>Home</span>
+                        </Link>
+                    </li>
+                    <li className={`${styles.item} ${styles.pags}`}>
+                        <Link to='/personagens' className={styles.link}>
+                            <img src={iconPerson} alt="Icon Person" className={styles.imgPerson} />
+                            <span>Personagens</span>
+                        </Link>
+                    </li>
+                    <li className={`${styles.item} ${styles.pags}`}>
+                        <Link to='/sobre' className={styles.link}>
+                            <img src={iconSobre} alt="Icon Sobre" className={styles.imgHome} />
+                            <span>Sobre</span>
+                        </Link>
+                    </li>
+                </ul>
+                <button className={`${styles.nav_btn} ${styles.nav_btn_close} ${styles.fechar}`} onClick={showBarNav}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button className={`${styles.nav_btn} ${styles.abrir}`} onClick={showBarNav}>
+                <FaBars />
+            </button>
+        </header>
     )
 }
 
--- action_max/src/Components/Pages/Sobre.js
+++ action_max/src/Components/Pages/Sobre.js
@@ -1,5 +1,11 @@
 function Sobre(){
-    return <h1>Sobre</h1>
+    return (
+        <>
+            <h1>Projeto Action Max </h1>
+            <p>Projeto desenvolvido para a disciplina de Programação Web 2 do curso de Sistemas de Informação da IFCE - Campus Cedro, com o objetivo de desenvolver um aplicação web que consuma uma API, na qual foi utilizada a API da Marvel</p>
+            <h2>Desenvolvido por: Isaac Freires </h2>
+        </>
+    )
 }
 
 export default Sobre
